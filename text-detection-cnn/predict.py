fde3d65a10768c91fa3d3eb77e3cf5cdae5c1e8d
rashemihmih <mimih96@gmail.com>
2017-09-14 16:07:30 +0300

    fix time measurement

--- text-detection-cnn/predict.py
+++ text-detection-cnn/predict.py
@@ -6,16 +6,22 @@ import time
 model = load_model('my_model.h5')
 model.load_weights('my_model_weights.h5')
 while True:
-    start = time.time()
     try:
         path = input('img path: ')
+        start_time = time.time()
         image = imread(path, True, 'L')
     except OSError:
         print('OSError')
         continue
+    file_read_time = time.time()
     image = imresize(image, (224, 224))
     image = image.astype('float32')
     image /= 255
     image = image.reshape(1, 224, 224, 1)
     prediction = model.predict(image)
-    print(numpy.argmax(prediction), time.time() - start)
+    end_time = time.time()
+    print('has text' if numpy.argmax(prediction) == 1 else 'no text')
+    print('elapsed time (s): ', end_time - start_time)
+    print('calculation time (s): ', end_time - file_read_time)
+    print('file read time (s): ', file_read_time - start_time)
+    print()
