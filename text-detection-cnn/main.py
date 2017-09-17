5c973a25bd5ae65a924fac759e4ba4748bf467bf
rashemihmih <mimih96@gmail.com>
2017-09-17 16:15:12 +0300

    add models

--- text-detection-cnn/main.py
+++ text-detection-cnn/main.py
@@ -2,4 +2,4 @@ from my_model import fit
 from data import load_data
 
 x_train, y_train, x_test, y_test = load_data()
-fit(x_train, y_train, x_test, y_test)
+fit(x_train, y_train, x_test, y_test, 16, 200)
--- text-detection-cnn/vgg16_model.py
+++ text-detection-cnn/vgg16_model.py
@@ -1,14 +1,14 @@
-from keras import Input
-from keras.applications.inception_v3 import InceptionV3
+from keras.applications.vgg16 import VGG16
 from keras.layers import Dense, Flatten, Dropout
 from keras.models import Model
+from keras import Input
 import data
 
 
 def fit(x_train, y_train, x_test, y_test, batch_size, epochs):
-    inception_v3_model = InceptionV3(include_top=False, weights=None, input_shape=(data.img_size, data.img_size, 1))
+    vgg16_model = VGG16(include_top=False, weights=None, input_shape=(data.img_size, data.img_size, 1))
     model_in = Input(shape=(data.img_size, data.img_size, 1))
-    model_out = inception_v3_model(model_in)
+    model_out = vgg16_model(model_in)
     model_out = Flatten()(model_out)
     model_out = Dense(512, activation='relu')(model_out)
     model_out = Dropout(0.25)(model_out)
@@ -16,8 +16,8 @@ def fit(x_train, y_train, x_test, y_test, batch_size, epochs):
     model = Model(input=model_in, output=model_out)
     model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
     model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1, shuffle=True)
-    model.save('inception_v3_model.h5')
-    model.save_weights('inception_v3_model_weights.h5')
+    model.save('vgg16_model.h5')
+    model.save_weights('vgg16_model_weights.h5')
     scores = model.evaluate(x_test, y_test, verbose=0)
     print('Test accuracy: ' + scores[1])
     return model
