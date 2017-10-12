6b2e7cd0a4d73081173a332c78670bee58da2b11
rashemihmih <mihmih96@gmail.com>
2017-10-12 20:26:30 +0300

    print accuracy

--- text-detection-cnn/predict_test.py
+++ text-detection-cnn/predict_test.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
 
+THRESHOLD = 0.9
+
 home_dir = str(Path.home())
 my_model = load_model(home_dir + '/my_model/my_model.h5')
 test_data_dir = home_dir + '/test_data'
@@ -16,4 +18,5 @@ test_generator = datagen.flow_from_directory(test_data_dir,
                                              color_mode='grayscale',
                                              class_mode='binary',
                                              batch_size=batch_size)
-print(my_model.predict_generator(test_generator, steps, verbose=1))
+results = my_model.predict_generator(test_generator, steps)
+print("Accuracy: ", len([i for i in results if i[0] > THRESHOLD]) / len(results))
