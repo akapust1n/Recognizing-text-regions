bf78490855bfa1cca22c1e2f9a3d6a53da36687d
rashemihmih <mihmih96@gmail.com>
2017-10-19 02:19:39 +0300

    fit after fit

--- text-detection-cnn/fitmore.py
+++ text-detection-cnn/fitmore.py
@@ -1,19 +1,22 @@
-from sys import argv
 from pathlib import Path
+from sys import argv
 
+from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
 
-if len(argv) != 3:
-    print(f'usage: {argv[0]} data_dir epochs')
+if len(argv) != 6:
+    print(f'usage: {argv[0]} model_path data_path train_samples validation_samples epochs')
 home_dir = str(Path.home())
-data_dir = argv[1]
-epochs = int(argv[2])
-train_data_dir = home_dir + f'/data/{data_dir}/train'
-validation_data_dir = home_dir + f'/data/{data_dir}/validation'
+model_path = str(Path.home()) + '/' + argv[1]
+data_path = str(Path.home()) + '/' + argv[2]
+train_samples = int(argv[3])
+validation_samples = int(argv[4])
+epochs = int(argv[5])
+
+train_data_dir = data_path + '/train'
+validation_data_dir = data_path + '/validation'
 img_size = 224
 batch_size = 32
-train_samples = 40000
-validation_samples = 10000
 steps_per_epoch = train_samples // batch_size
 validation_steps = validation_samples // batch_size
 datagen = ImageDataGenerator(rescale=1. / 255)
@@ -27,3 +30,11 @@ validation_generator = datagen.flow_from_directory(validation_data_dir,
                                                    color_mode='grayscale',
                                                    class_mode='binary',
                                                    batch_size=batch_size)
+
+my_model = load_model(model_path)
+my_model.fit_generator(train_generator,
+                       steps_per_epoch=steps_per_epoch,
+                       epochs=epochs,
+                       validation_data=validation_generator,
+                       validation_steps=validation_steps)
+my_model.save(model_path[:-3] + '_fitmore.h5')
